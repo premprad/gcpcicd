910cbf40cf8ada9ddc2cfcf515bf86ca92fd7bc4
Dasprakash <daspprem86@gmail.com>
2025-09-15 09:17:04 +0000

    update cloudbuild

--- app.py
+++ app.py
@@ -15,4 +15,5 @@ if __name__ == "__main__":
     # Used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
     # can be configured by adding an `entrypoint` to app.yaml.
-    app.run(host="localhost", port=8080, debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port, debug=True)
